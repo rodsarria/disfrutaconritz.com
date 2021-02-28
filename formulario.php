7a9bd90ea0de8498a97a480713a7776e17018de8
Carlos Castillo <icochea1@gmail.com>
2021-02-27 21:55:35 -0500

    nepe

--- formulario.php
+++ formulario.php
@@ -15,46 +15,51 @@
     $gano = false;
     $found = false;
 
-    session_start();
-    $_SESSION['gano'] = "no";
-    $_SESSION['id'] = "";
-
-    $sql = "SELECT id,nombre,DNI,telefono,factura,fechaCompra,montoTicket,fecha,gano FROM participante WHERE DNI='$DNI'";
-
-    if ($result = $mysqli->query($sql))
+    if(isset($_POST['submit'])) 
     {
-        if ($result->num_rows > 0)
+        session_start();
+        $_SESSION['gano'] = "no";
+        $_SESSION['id'] = "";
+
+        $sql = "SELECT id,nombre,DNI,telefono,factura,fechaCompra,montoTicket,fecha,gano FROM participante WHERE DNI='$DNI'";
+
+        if ($result = $mysqli->query($sql))
         {
-            while($row = $result->fetch_array())
+            if ($result->num_rows > 0)
             {
-                 if($row[7]=="Si")
-                 {
-                    $gano = true;
-                 }
+                while($row = $result->fetch_array())
+                {
+                     if($row[7]=="Si")
+                     {
+                        $gano = true;
+                     }
 
-                 $found = true;
+                     $found = true;
 
-                 $_SESSION['id'] = $row[0];
-                 break;
+                     $_SESSION['id'] = $row[0];
+                     break;
 
+                }
             }
         }
-    }
 
-    $_SESSION['gano']  = $gano;
+        $_SESSION['gano']  = $gano;
 
-    if($found==false)
-    {
-        $sql2 = "INSERT INTO participante (nombre,DNI,telefono,factura,fechaCompra,montoTicket) VALUES ('$name','$DNI','$phone','$ticket','$date','$amount')";
-
-        if ($mysqli->query($sql2) === true)
+        if($found==false)
         {
-            $lastID = $mysqli->insert_id;
-            $_SESSION['id'] = $lastID;
+            $sql2 = "INSERT INTO participante (nombre,DNI,telefono,factura,fechaCompra,montoTicket) VALUES ('$name','$DNI','$phone','$ticket','$date','$amount')";
+
+            if ($mysqli->query($sql2) === true)
+            {
+                $lastID = $mysqli->insert_id;
+                $_SESSION['id'] = $lastID;
+            }
         }
+        
+        echo ' <script type="text/javascript">window.location.href = "raspa.php";</script>';
     }
+
     
-    echo ' <script type="text/javascript">window.location.href = "raspa.php";</script>';
 ?>
 
 <!DOCTYPE html>
